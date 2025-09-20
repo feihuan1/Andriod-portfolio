ad50e2c567a52aa13690a627cf5f24515ac89b34
Feihuan Peng <feihuan.peng@spreetail.com>
2025-09-20 01:09:18 -0500

    asd

--- app/src/main/java/com/example/andoroiddevportfolio/MainActivity.java
+++ app/src/main/java/com/example/andoroiddevportfolio/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Project[] projects = {
+                new Project("Getting started app", "My very first project 'Hello world app",R.drawable.getting_started),
+                new Project("Vin Quote", "Making simple change to the layout",R.drawable.quote),
+                new Project("BMI Calculator", "A real life working BMI calculator calculates BMI base on age and gender height and body weight",R.drawable.calculator),
+                new Project("Inches Convertor", "Working app to convert inches to meters",R.drawable.tape),
+                new Project("The hungry Dev", "A restaurant menu app, learning about Listview and Scrollview, activities and classes",R.drawable
+                        .hungry_developer)
+        };
     }
+
 }
---
+++ app/src/main/java/com/example/andoroiddevportfolio/Project.java
@@ -0,0 +1,15 @@
+package com.example.andoroiddevportfolio;
+
+public class Project {
+    String name;
+    String description;
+    // R.drawable.name_of_the_file this will be some sort of int e.g. 15638263)
+    int image;
+
+    public Project(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+}
